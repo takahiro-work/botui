7e7bb4058518da71a50dc6dd81f87f0bf6df4924
datapf-osaka <work.t.hys.24@gmail.com>
2021-09-30 14:33:22 +0900

    update

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+// CORSを有効にする
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+app.get('/chat', function (req, res) {
+    const userInputText = req.query.text;
+    const callback = req.query.callback;
+    const response = {output: []};
+    const msg = response.output;
+    msg.push({
+        type: 'text',
+        value: '「' + userInputText + '」ですね！わはは！'
+    });
+    if (callback) {
+        const responseText = callback + '(' + JSON.stringify(response) + ')';
+        res.set('Content-Type', 'application/javascript');
+        res.send(responseText);
+
+    } else {
+        res.json(response);
+    }
+});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+}); 
+app.listen(process.env.PORT || port, () => {
+    console.log('チャットサーバーを開始しました ポート番号:' + port);
+});
