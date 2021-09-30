3b2131feabab425db38ba877bbf16674c1230752
datapf-osaka <work.t.hys.24@gmail.com>
2021-09-30 15:01:41 +0900

    update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 // CORSを有効にする
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+//     next();
+// });
 app.get('/chat', function (req, res) {
     const userInputText = req.query.text;
     const callback = req.query.callback;
